a62433d67478a9e42da469f27d780f5c1f18340e
lorypelli <lorypelli@proton.me>
2025-06-17 15:04:17 +0200

    first commit

---
+++ astro.config.js
@@ -0,0 +1,11 @@
+import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    adapter: cloudflare(),
+});
