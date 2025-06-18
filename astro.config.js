426b9d740469b7db2ff08b8b0b8d4640ca3fa479
lorypelli <lorypelli@proton.me>
2025-06-18 13:19:43 +0200

    fix react

--- astro.config.js
+++ astro.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
     output: 'server',
     vite: {
         plugins: [tailwindcss()],
+        resolve: {
+            alias: import.meta.env.PROD
+                ? { 'react-dom/server': 'react-dom/server.edge' }
+                : {},
+        },
     },
     adapter: cloudflare(),
     integrations: [react()],
