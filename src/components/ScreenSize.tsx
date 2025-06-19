944999776cfc6d6f49a776ab588456bfb8ad8717
lorypelli <lorypelli@proton.me>
2025-06-19 09:23:28 +0200

    use dotted border

--- src/components/ScreenSize.tsx
+++ src/components/ScreenSize.tsx
@@ -3,7 +3,7 @@ import { useWindowSize } from '@uidotdev/usehooks';
 export default () => {
     const { width, height } = useWindowSize();
     return (
-        <div className="flex flex-col items-center rounded-2xl border-2 p-2 shadow-2xl sm:p-4 sm:text-3xl">
+        <div className="flex flex-col items-center rounded-2xl border-2 border-dotted p-2 shadow-2xl sm:p-4 sm:text-3xl">
             <div className="flex gap-x-1">
                 <span>Width:</span>
                 <span className="font-bold">{width}</span>
